82b3f8f899a4bda51c8b7592746a905814ac98e4
Isak Larsson <isaklarsson@McIsak.local>
2023-08-11 13:21:16 +0200

    fixed bug regarding karabiner assets path

--- updatefiles.sh
+++ updatefiles.sh
@@ -6,4 +6,4 @@ ln -s $current_cirectory/.tmux.conf ~/.tmux.conf
 ln -s $current_directory/starship.toml ~/.config/starship.toml
 ln -s $current_directory/.zshrc ~/.zshrc
 ln -s $current_directory/karabiner.json ~/.config/karabiner/karabiner.json
-ln -s $current_directory/assets ~/.config/karabiner/assets
+ln -s $current_directory/assets ~/.config/karabiner/
