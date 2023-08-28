f7c6854a866e93d9c094d098b8d25af0976d433f
Isak Larsson <isaklarsson@McIsak.local>
2023-08-28 13:21:38 +0200

    cleaned up install part

--- setup.sh
+++ setup.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 echo "Remember to install iterm as well :)"
 echo "*** Setting up your stuff for you :) ***"
 
@@ -6,24 +8,21 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/too
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 
-echo "*** Installing stuff ***"
+echo "*** Installing tools ***"
 
-brew install gh
-brew install starship
-brew install neovim
-brew install tmux
-brew install --cask karabiner-elements
-brew install bat
-brew install node
-brew install fzf
-brew install lazygit
-brew install exa
+tools=("gh" "starship" "neovim" "tmux" "--cask karabiner-elements" "bat" "node" "fzf" "lazygit" "exa")
+for TOOL in "${tools[@]}"; do
+do
+  brew install $TOOL
+done
+
 $(brew --prefix)/opt/fzf/install
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
-echo "*** Configuring Neovim ***"
-#NvChad custom setup
 
+echo "*** Configuring Neovim ***"
+
+#NvChad custom setup
 git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth 1 && nvim
 git clone https://github.com/IsakLarsson/nvchad-custom.git ~/.config/nvchad-custom
 rm -rf ~/.config/nvim/lua/custom
