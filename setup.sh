fd10e1a14bb2f1355e3b92879edbdcfa37ab5764
Isak Larsson <isaklarsson98@hotmail.com>
2023-09-02 17:25:10 +0200

    added github login

--- setup.sh
+++ setup.sh
@@ -6,7 +6,12 @@ echo "*** Setting up your stuff for you :) ***"
 xcode-select --install
 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+brew install gh
+gh auth login
+
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 
